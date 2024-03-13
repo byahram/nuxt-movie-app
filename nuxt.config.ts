9255f4d13d2f6f5876ed1d19ab060f0e35a0e959
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-03-13 15:51:58 +0900

    tailwindcss && nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-icon"],
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      primary: "Open Sans",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1192px",
+    },
+    extend: {
+      colors: {
+        light: "#fff",
+        dark: "#111",
+      },
+      height: {
+        fill: "-webkit-fill-available",
+      },
+    },
+  },
+  plugins: [],
+};
